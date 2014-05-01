a7692c8d7cd106e657ca44c1da95a2dd673247b6
William Candillon <wcandillon@gmail.com>
2014-05-01 15:59:04 +0200

    Add deploy grunt task.

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    require('load-grunt-tasks')(grunt);
+   
+    var path = require('path');
+ 
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        'gh-pages': {
+            docs: {
+                src: '**/*',
+                options: {
+                    base: '_book'
+                }
+            }
+        }, 
+        gitbook: {
+          development: {
+            output: path.join(__dirname, '_book'),
+            input: __dirname,
+            title: 'JSONiq Cookbook',
+            github: '28msec/jsoniq-cookbook'
+          }
+        }
+    });
+
+    grunt.registerTask('default', ['gitbook', 'gh-pages']);
+};
